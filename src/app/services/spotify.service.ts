2e716662e8cf9dcc0ed3e65526aeb1d42733b2f0
FabianRausch <fabianr95.fr@gmail.com>
2020-05-20 22:43:31 -0300

    first commit

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) { 
+    console.log('Spotify Service Listo');
+  }
+
+ getQuery( query: string ) {
+
+  const url = `https://api.spotify.com/v1/${ query }`;
+
+  const headers = new HttpHeaders({
+    'Authorization': 'Bearer BQCl3NiZo-FKhd9xkNK5iVU_5mko1TNShC0KGUMmSnSw9ADyf3SfxOjKnSvlLkP9cMzMHIFyQOPikL2L3ns'
+  });
+
+  return this.http.get(url, { headers });
+
+
+
+ }
+
+  getNewReleases() {
+
+
+    return this.getQuery('browse/new-releases?limit=20')
+              .pipe( map( data => data['albums'].items ) );
+      
+  }
+
+    getArtistas( termino: string ) {
+
+      return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+                .pipe( map( data => data['artists'].items) );
+    
+    }
+
+    getArtista( id: string ) {
+
+      return this.getQuery(`artists/${ id }`);
+              //  .pipe( map( data => data['artists'].items) );
+    
+    }
+
+    getTopTracks( id: string ) {
+
+      return this.getQuery(`artists/${ id }/top-tracks?country=us`)
+                .pipe( map( data => data['tracks']) );
+    
+    }
+
+}
